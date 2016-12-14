e6ca73f110a6d52a0fde7bb695d309f1f2b178ae
Frank <fjlanasa@gmail.com>
2016-12-14 13:47:46 -0500

    changed file names of reducers

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,8 +2,8 @@ import { combineReducers } from 'redux';
 import { routerReducer } from 'react-router-redux';
 
 import searchTerm from './searchTerm'
-import searchResults from './search'
-import definitions from './save'
+import searchResults from './searchResults'
+import definitions from './definitions'
 
 const rootReducer = combineReducers({searchResults, definitions, searchTerm, routing: routerReducer});
 
